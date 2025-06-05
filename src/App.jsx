0c54e56d194da1038230a2a24bf477b5197fb119
Pranav-Singh16 <pranavsingh16ps@gmail.com>
2025-06-05 23:52:04 +0530

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import Navbar from "./components/navbar-mobile-laptop";
+
 function App() {
-  return <div className="bg-red-400 text-3xl font-bold underline">welcome</div>;
+  return (
+    <div>
+      <div className="hidden lg:block">
+        <nav className="bg-red-400">Nav</nav>
+        <span className="flex">
+          <div className="bg-yellow-400 w-1/2">Aside</div>
+          <div className="bg-blue-400 w-1/2">Main</div>
+        </span>
+      </div>
+
+      <div className="lg:hidden">
+        <nav className="bg-red-400">Nav</nav>
+        <div className="bg-blue-400">Main</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
